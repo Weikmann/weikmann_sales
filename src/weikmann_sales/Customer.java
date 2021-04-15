dc02f041e9126ba504a41668f2bc862f6766de64
MartinW <wei21921@spengergasse.at>
2021-04-16 01:05:54 +0200

    Weikmann: Added class Customer

---
+++ src/weikmann_sales/Customer.java
@@ -0,0 +1,28 @@
+package weikmann_sales;
+
+public class Customer {
+
+	private int customerID;
+	private String firstName;
+	
+	public Customer(int customerID, String firstName) {
+		super();
+		this.customerID = customerID;
+		this.firstName = firstName;
+	}
+	
+	
+	public int getCustomerID() {
+		return customerID;
+	}
+	public void setCustomerID(int customerID) {
+		this.customerID = customerID;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	
+}
