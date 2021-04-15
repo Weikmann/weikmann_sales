e95bf00b84c0ccb0372a4c7d302abd36984e1cec
MartinW <wei21921@spengergasse.at>
2021-04-16 01:21:19 +0200

    Weikmann: added Class Customers (ArrayList)

---
+++ src/weikmann_sales/Customers.java
@@ -0,0 +1,14 @@
+package weikmann_sales;
+
+import java.util.ArrayList;
+
+public class Customers {
+	
+	ArrayList<Customer> customers;
+
+	public Customers() {
+		customers = new ArrayList<>();
+	}
+	
+
+}
